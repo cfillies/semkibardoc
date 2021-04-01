a9a8a187c2ca9a837f964a5e188bbbbce890f7d5
Christian Fillies <cfillies@hotmail.com>
2021-04-01 10:05:19 +0200

    c

--- test_app/hello1.py
+++ test_app/hello1.py
@@ -5,7 +5,6 @@ import json
 
 uri = "mongodb+srv://semtation:SemTalk3!@cluster0.pumvg.mongodb.net/kibardoc?retryWrites=true&w=majority" 
 
-# uri = "mongodb://semtalk-cosmos-mongo:XM09lEM9ptbh8o1cHDOQnpgjJ4nZ19Hm1BvdnKTubsizIZJeqGlE56n8BZwDkuJEhTsyHweVEH8T0adJkm50bQ==@semtalk-cosmos-mongo.documents.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb"
 # uri = "mongodb://localhost:27017"
 myclient = pymongo.MongoClient(uri)
 myclient._topology_settings
