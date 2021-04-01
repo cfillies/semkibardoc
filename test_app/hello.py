f8800bb949ee40a3bcfbbefb8d4eff951ec29bc8
Christian Fillies <cfillies@hotmail.com>
2021-04-01 15:25:03 +0200

    ccc

--- test_app/hello.py
+++ test_app/hello.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, json,Response
+import pymongo
+import os
 
 myapp = Flask(__name__)
 
@@ -8,5 +10,23 @@ def hello():
 
 @myapp.route("/foo")
 def foo():
-    f = { "foo": "Hello Flask, on Azure App Service for Linux" }
-    return f
+    uri = os.getenv("MONGO_CONNECTION")
+    myclient = pymongo.MongoClient(uri)
+    myclient._topology_settings
+    mydb = myclient["kibardoc"]
+    collist = mydb.list_collection_names()
+    vi = {}
+    if "vorhaben_inv" in collist:
+        vorhabeninv_col = mydb["vorhaben_inv"]
+        vorhabeninv = vorhabeninv_col.find()
+        for v in vorhabeninv:
+            print(v["intents"])
+            print(v["words"])
+            for int in v["intents"]:
+                vi[int] = v["intents"][int]
+
+    # f = { "foo": "Hello Flask, on Azure App Service for Linux" }
+    json_string = json.dumps(vi,ensure_ascii = False)
+    #creating a Response object to set the content type and the encoding
+    response = Response(json_string,content_type="application/json; charset=utf-8" )
+    return response
--- test_app/hello1.py
+++ test_app/hello1.py
@@ -1,11 +1,12 @@
 # import spacy
 import pymongo
-import json
+import os
 
 
-uri = "mongodb+srv://semtation:SemTalk3!@cluster0.pumvg.mongodb.net/kibardoc?retryWrites=true&w=majority" 
 
-# uri = "mongodb://localhost:27017"
+# uri = "mongodb+srv://semtation:SemTalk3!@cluster0.pumvg.mongodb.net/kibardoc?retryWrites=true&w=majority" 
+
+uri = "mongodb://localhost:27017"
 myclient = pymongo.MongoClient(uri)
 myclient._topology_settings
 
