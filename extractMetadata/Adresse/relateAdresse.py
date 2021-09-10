3c64901adbe155763dce2eda2a004f852c247156
Jonas König <jonas.koenig@msg.group>
2021-09-10 14:04:09 +0200

    Fixed imports in relateAdresse.py

--- extractMetadata/Adresse/relateAdresse.py
+++ extractMetadata/Adresse/relateAdresse.py
@@ -1,24 +1,25 @@
-import extract.extractAdresse as extractAdresse
-import extract.extractText as extractText
 from typing import Dict
 
-def findAddress(metadata: Dict, parser: str):
+from extractMetadata.extract.extractAdresse import getAdresse
+from extractMetadata.extract.extractText import getTextContent
 
+
+def findAddress(metadata: Dict, parser: str):
     pfad = next(iter(metadata))
     datei = next(iter(metadata[pfad]))
     inhalt = metadata[pfad][datei]['inhalt']
-    
+
     # Pfad
     # TODO pass getAdresse only the filepath starting at the data parent dir, not C:\\users\\...
     #   to shorten regex search time
-    adressen, adresse, adrName = extractAdresse.getAddress(pfad)
+    adressen, adresse, adrName = getAdresse(pfad)
     # Dateiname
     if not adressen:
-        adressen, adresse, adrName = extractAdresse.getAddress(datei)
+        adressen, adresse, adrName = getAdresse(datei)
     # Inhalt
     if not adressen:
         if inhalt == '':
-            inhalt = extractText.getTextContent(metadata, parser)
+            inhalt = getTextContent(metadata, parser)
 
-        adressen, adresse, adrName = extractAdresse.getAddress(inhalt) #inhalt = inhalt.split('§')[0]
+        adressen, adresse, adrName = getAdresse(inhalt)  # inhalt = inhalt.split('§')[0]
     return adressen, adresse, adrName
